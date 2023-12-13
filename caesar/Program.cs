f26fb3ba184feff4773a000db3298282bd43fb0d
Owain Williams <owain@hedfan.uk>
2023-12-13 23:08:26 +0000

    Create a Caesar project for trying Caesar cyphers

---
+++ caesar/Program.cs
@@ -0,0 +1,25 @@
+﻿using System.Text;
+
+var cyphertext = args[0].ToUpperInvariant();
+
+Console.WriteLine($" 0 {cyphertext}");
+
+for (int k = 1; k < 26; k++)
+{
+    var plaintext = new StringBuilder(cyphertext.Length);
+    for (int i = 0; i < cyphertext.Length; i++)
+    {
+        var c = cyphertext[i];
+        if (Char.IsLetter(c))
+        {
+            plaintext.Append((char)(((c - 65 + k) % 26) + 65));
+        }
+        else
+        {
+            plaintext.Append(c);
+        }
+    }
+
+    Console.WriteLine($"{k,2} {plaintext.ToString().ToLowerInvariant()}");
+
+}

f547fad4c8637bffc7624d483b35a831955d4456
Owain Williams <owain@hedfan.uk>
2024-01-03 00:27:03 +0000

    Register an IWords fixture using EnglishAlphaWords
    Neither IWords nor EnglishAlphaWords exist yet, we'll create these later.

--- test/WordsTests/WordsTests.cs
+++ test/WordsTests/WordsTests.cs
@@ -2,6 +2,13 @@ namespace WordsTests;
 
 public class WordsTests
 {
+    private readonly IFixture _fixture = new Fixture();
+
+    public WordsTests()
+    {
+        _fixture.Register<IWords>(() => new EnglishAlphaWords());
+    }
+
     [Fact]
     public void Test1()
     {

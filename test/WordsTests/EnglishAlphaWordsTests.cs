692b1465f9d6c1f31dbb05ed5b4e1d0af3e89494
Owain Williams <owain@hedfan.uk>
2024-01-03 13:25:58 +0000

    Add a test to check a made up word does not exist in the collection

--- test/WordsTests/EnglishAlphaWordsTests.cs
+++ test/WordsTests/EnglishAlphaWordsTests.cs
@@ -15,4 +15,17 @@ public class EnglishAlphaWordsTests
         // Assert
         result.Should().BeTrue();
     }
+
+    [Fact]
+    public void ContainsShouldReturnFalseIfWordDoesNotExistsInCollection()
+    {
+        // Arrange
+        var sut = new EnglishAlphaWords();
+
+        // Act
+        var result = sut.Contains("zgxsmnmfrkskagpvhpiulvhagrg");
+
+        // Assert
+        result.Should().BeFalse();
+    }
 }

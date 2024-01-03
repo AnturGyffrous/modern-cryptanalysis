4845c0969d04cc8b05164cb21018b5ecd57937ab
Owain Williams <owain@hedfan.uk>
2024-01-03 11:57:43 +0000

    More WIP

--- src/Words/EnglishAlphaWords.cs
+++ src/Words/EnglishAlphaWords.cs
@@ -2,4 +2,17 @@ namespace Words;
 
 public class EnglishAlphaWords : ICollection<string>
 {
+    private ICollection<string> _list = new List<string();
+
+    public EnglishAlphaWords()
+    {
+        using (var reader = new StreamReader("../../english-words/words_alpha.txt"))
+        {
+            string line;
+            while ((line = reader.ReadLine()) != null)
+            {
+                _list.Add(line);
+            }
+        }
+    }
 }

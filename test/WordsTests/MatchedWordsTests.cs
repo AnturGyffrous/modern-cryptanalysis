a727e9cfabe8bb0556e3e6d9f6bdf77575cd8ddd
Owain Williams <owain@hedfan.uk>
2024-01-04 00:37:44 +0000

    Call calculate on MatchedWords

--- test/WordsTests/MatchedWordsTests.cs
+++ test/WordsTests/MatchedWordsTests.cs
@@ -8,5 +8,8 @@ public class MatchedWordsTests
         // Arrange
         var words = new List<string> { "the", "quick", "brown", "fox", "jumps", "over", "lazy", "dog" };
         var sut = new MatchedWords(words);
+
+        // Act
+        var result = sut.Calculate("the quick brown fox jumps over the lazy dog");
     }
 }

84a5ac0055470fdc3406be33ebef966a58c496d1
Owain Williams <owain@hedfan.uk>
2024-01-04 00:40:41 +0000

    Add a test for no matching words

--- test/WordsTests/MatchedWordsTests.cs
+++ test/WordsTests/MatchedWordsTests.cs
@@ -11,5 +11,22 @@ public class MatchedWordsTests
 
         // Act
         var result = sut.Calculate("the quick brown fox jumps over the lazy dog");
+
+        // Assert
+        result.Should().Be(1);
+    }
+
+    [Fact]
+    public void CalculateShouldBeZeroPercentIfNoWordsMatch()
+    {
+        // Arrange
+        var words = new List<string> { "the", "quick", "brown", "fox", "jumps", "over", "lazy", "dog" };
+        var sut = new MatchedWords(words);
+
+        // Act
+        var result = sut.Calculate("iwt fjxrz qgdlc udm yjbeh dktg iwt apon sdv");
+
+        // Assert
+        result.Should().Be(0);
     }
 }

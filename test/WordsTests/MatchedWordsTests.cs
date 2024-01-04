01e3092b47c15828c0fc8e817a762316437024fd
Owain Williams <owain@hedfan.uk>
2024-01-04 00:29:04 +0000

    Add a test for all words matching

--- test/WordsTests/MatchedWordsTests.cs
+++ test/WordsTests/MatchedWordsTests.cs
@@ -2,4 +2,11 @@ namespace WordsTests;
 
 public class MatchedWordsTests
 {
+    [Fact]
+    public void CalculateShouldBeOneHundredPercentIfAllWordsMatch()
+    {
+        // Arrange
+        var words = new List<string> { "the", "quick", "brown", "fox", "jumps", "over", "lazy", "dog" };
+        var sut = new MatchedWords(words);
+    }
 }
